43038f5e232477e29f69bad7a693720035e4f30b
Shyam Gupta <postboxshyam@gmail.com>
2022-11-20 06:36:52 -0500

    Corrected Typo

--- vpc.tf
+++ vpc.tf
@@ -123,7 +123,6 @@ provider "aws" {
 }
 # Create EC2 in Private subnet
 
-    # Create an EC2 instance in public subnet
     resource "aws_instance" "ec2_private"{
         ami = "ami-0149b2da6ceec4bb0"
         instance_type = "t2.micro"
@@ -147,4 +146,8 @@ provider "aws" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     }
+}
+
+output "ec2_public_ip"{
+    value = aws_instance.my_ec2.public_ip
 }
